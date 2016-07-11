93cc1cc0863d8cfb77d3c77c1ef77156a59036e4
Ethan Ward <ethanandrewward@gmail.com>
2016-07-11 16:58:50 -0500

    Update test_runner.py

--- test_runner.py
+++ test_runner.py
@@ -4,12 +4,9 @@ import unittest
 from os.path import dirname
 from xmlrunner import XMLTestRunner
 
-from mycroft.configuration import ConfigurationManager
-
 __author__ = 'seanfitz, jdorleans'
 if __name__ == "__main__":
     fail_on_error = "--fail-on-error" in sys.argv
-    ConfigurationManager.load_local(['mycroft.ini'])
 
     tests = unittest.TestLoader().discover(dirname(__file__), "*.py")
     runner = XMLTestRunner("./build/report/tests")
